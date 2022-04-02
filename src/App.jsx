b688de1eb41e37de32742102da747c75157eb4c6
denis <182906@unsaac.edu.pe>
2022-04-01 22:58:09 -0500

    Add mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -56,15 +56,19 @@ const App = () => {
 
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center justify-center bg-[#AF67E9] pt-[120px] pb-[50px]">
-        <div className="flex h-[535px] w-[330px] flex-col items-center rounded-3xl bg-white px-4 font-kumbhSans">
+      <div className="flex min-h-screen flex-col items-center justify-center bg-[#AF67E9] pt-[120px] pb-[50px] shadow-[#6565E7]">
+        <div className="flex h-[535px] w-[330px] flex-col items-center rounded-3xl bg-white px-7 font-kumbhSans drop-shadow-2xl">
           <Icon height={200} width={230} className="relative bottom-[115px]" />
-          <h1 className="mb-8 -mt-24 text-center text-3xl font-bold">FAQ</h1>
+          <h1 className="-mt-16 mb-7 text-center text-3xl font-bold">FAQ</h1>
 
           {/* Question 1 */}
           <div className="flex w-full flex-col gap-4">
             <div className="flex items-center justify-between text-[14px]">
-              <p className={`${qOne ? "font-bold" : "font-normal"}`}>
+              <p
+                className={`${
+                  qOne ? "font-bold" : "font-normal"
+                } text-[#4A4B5E]`}
+              >
                 How many team members can I invite?
               </p>
               <img
@@ -76,16 +80,24 @@ const App = () => {
               />
             </div>
             {qOne && (
-              <p className="transform text-[12px] transition duration-1000 ease-out">
+              <p className="relative bottom-2 transform pr-[5px] text-[12px] text-[#787887]">
                 You can invite up to 2 additional users on the Free plan. There
                 is no limit on team members for the Premium plan.
               </p>
             )}
-            <div className="h-[1px] w-full bg-[#E7E7E9]"></div>
+            <div
+              className={`h-[1px] w-full bg-[#E7E7E9] ${
+                qOne ? "relative -mt-3" : ""
+              }`}
+            ></div>
 
             {/* Question 2 */}
             <div className="flex items-center justify-between text-[14px]">
-              <p className={`${qTwo ? "font-bold" : "font-normal"}`}>
+              <p
+                className={`${
+                  qTwo ? "font-bold" : "font-normal"
+                } text-[#4A4B5E]`}
+              >
                 What is the maximum file upload size?
               </p>
               <img
@@ -97,16 +109,24 @@ const App = () => {
               />
             </div>
             {qTwo && (
-              <p className="transform text-[12px] transition duration-1000 ease-out">
+              <p className="relative bottom-2 transform pr-[5px] text-[12px] text-[#787887]">
                 No more than 2GB. All files in your account must fit your
                 allotted storage space.
               </p>
             )}
-            <div className="h-[1px] w-full bg-[#E7E7E9]"></div>
+            <div
+              className={`h-[1px] w-full bg-[#E7E7E9] ${
+                qTwo ? "relative -mt-3" : ""
+              }`}
+            ></div>
 
             {/* Question 3 */}
             <div className="flex items-center justify-between text-[14px]">
-              <p className={`${qThree ? "font-bold" : "font-normal"}`}>
+              <p
+                className={`${
+                  qThree ? "font-bold" : "font-normal"
+                } text-[#4A4B5E]`}
+              >
                 How do I reset my password?
               </p>
               <img
@@ -118,16 +138,24 @@ const App = () => {
               />
             </div>
             {qThree && (
-              <p className="transform text-[12px] transition duration-1000 ease-out">
+              <p className="relative bottom-2 transform pr-[5px] text-[12px] text-[#787887]">
                 Click “Forgot password” from the login page or “Change password”
                 from your profile page. A reset link will be emailed to you.
               </p>
             )}
-            <div className="h-[1px] w-full bg-[#E7E7E9]"></div>
+            <div
+              className={`h-[1px] w-full bg-[#E7E7E9] ${
+                qThree ? "relative -mt-3" : ""
+              }`}
+            ></div>
 
             {/* Question 4 */}
             <div className="flex items-center justify-between text-[14px]">
-              <p className={`${qFour ? "font-bold" : "font-normal"}`}>
+              <p
+                className={`${
+                  qFour ? "font-bold" : "font-normal"
+                } text-[#4A4B5E]`}
+              >
                 Can I cancel my subscription?
               </p>
               <img
@@ -139,16 +167,24 @@ const App = () => {
               />
             </div>
             {qFour && (
-              <p className="transform text-[12px] transition duration-1000 ease-out">
+              <p className="relative bottom-2 transform pr-[5px] text-[12px] text-[#787887]">
                 Yes! Send us a message and we'll process your request no
                 questions asked.
               </p>
             )}
-            <div className="h-[1px] w-full bg-[#E7E7E9]"></div>
+            <div
+              className={`h-[1px] w-full bg-[#E7E7E9] ${
+                qFour ? "relative -mt-3" : ""
+              }`}
+            ></div>
 
             {/* Question 5 */}
             <div className="flex items-center justify-between text-[14px]">
-              <p className={`${qFive ? "font-bold" : "font-normal"}`}>
+              <p
+                className={`${
+                  qFive ? "font-bold" : "font-normal"
+                } text-[#4A4B5E]`}
+              >
                 Do you provide additional support?
               </p>
               <img
@@ -160,12 +196,16 @@ const App = () => {
               />
             </div>
             {qFive && (
-              <p className="transform text-[12px] transition duration-1000 ease-out">
+              <p className="relative bottom-2 transform pr-[5px] text-[12px] text-[#787887]">
                 Chat and email support is available 24/7. Phone lines are open
                 during normal business hours.
               </p>
             )}
-            <div className="h-[1px] w-full bg-[#E7E7E9]"></div>
+            <div
+              className={`h-[1px] w-full bg-[#E7E7E9] ${
+                qFive ? "relative -mt-3" : ""
+              }`}
+            ></div>
           </div>
         </div>
       </div>
